24261d607d55f64ca7f501d58b88dfc151ce3a54
agatha <sagatha602@gmail.com>
2024-01-31 16:06:47 -0300

    Colocando as classes mamiferos e animal

---
+++ src/model/Animal.ts
@@ -0,0 +1,37 @@
+class Animal {
+
+    private nome: string;
+    private idade: number;
+    private genero: string;
+
+    constructor(_nome: string,
+                 _idade: number,
+                  _genero: string) {
+        this.nome = _nome;
+        this.idade = _idade
+        this.genero = _genero                
+    }
+    public getNome(): string {
+        return this.nome;
+    }
+
+    public setNome(nome: string) {
+        this.nome = nome;
+    }
+
+    public getIdade(): number {
+        return this.idade;
+    }
+
+    public setIdade(idade: number) {
+        this.idade = idade;
+    }
+
+    public getGenero(): string {
+        return this.genero;
+    }
+
+    public setGenero(genero: string) {
+        this.genero = genero;
+    }
+}
---
+++ src/model/Mamifero.ts
@@ -0,0 +1,11 @@
+class Mamifero extends Animal {
+    private raca: string;
+
+    constructor(_raca: string,
+                 _nome: string,
+                 _idade: number,
+                 _genero: string,){
+        super(_nome, _idade, _genero);
+        this.raca = _raca;
+    }
+}
